fcd24fb7e7ebd0246c0dba9211d04f2e4c67af54
TongLong-UMP <tongai.kabaira@gmail.com>
2025-06-30 22:01:45 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -3,132 +3,96 @@
 DDL Script: Create Silver Tables
 ===============================================================================
 Script Purpose:
-    This script creates tables in the 'silver' schema, dropping existing tables
+    This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
-    Run this script to re-define the DDL structure of 'silver' Tables
+	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
-
-USE [DataWarehouse]
-GO
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_cust_info;
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [silver].[crm_cust_info](
-    [cst_id] [varchar](50) NOT NULL,
-    [cst_key] [varchar](50) NULL,
-    [cst_firstname] [varchar](100) NULL,
-    [cst_lastname] [varchar](100) NULL,
-    [cst_marital_status] [varchar](10) NULL,
-    [cst_gndr] [varchar](10) NULL,
-    [cst_create_date] [datetime] NULL
-) ON [PRIMARY]
+CREATE TABLE silver.crm_cust_info (
+    cst_id             INT,
+    cst_key            NVARCHAR(50),
+    cst_firstname      NVARCHAR(50),
+    cst_lastname       NVARCHAR(50),
+    cst_marital_status NVARCHAR(50),
+    cst_gndr           NVARCHAR(50),
+    cst_create_date    DATE,
+    dwh_create_date    DATETIME2 DEFAULT GETDATE()
+);
 GO
 
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
     DROP TABLE silver.crm_prd_info;
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [silver].[crm_prd_info](
-    [prd_id] [int] NULL,
-    [cat_id] [nvarchar](50) NULL,
-    [prd_key] [nvarchar](50) NULL,
-    [prd_nm] [nvarchar](50) NULL,
-    [prd_cost] [int] NULL,
-    [prd_line] [nvarchar](50) NULL,
-    [prd_start_dt] [date] NULL,
-    [prd_end_dt] [date] NULL,
-    [dwh_create_date] [datetime2](7) NULL
-) ON [PRIMARY]
+CREATE TABLE silver.crm_prd_info (
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE silver.crm_sales_details;
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [silver].[crm_sales_details](
-    [sls_ord_num] [nvarchar](50) NULL,
-    [sls_prd_key] [nvarchar](50) NULL,
-    [sls_cust_id] [int] NULL,
-    [sls_order_dt] [date] NULL,
-    [sls_ship_dt] [date] NULL,
-    [sls_due_dt] [date] NULL,
-    [sls_sales] [int] NULL,
-    [sls_quantity] [int] NULL,
-    [sls_price] [int] NULL,
-    [dwh_create_date] [datetime2](7) NULL
-) ON [PRIMARY]
-GO
-
-IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE silver.erp_cust_az12;
-GO
-
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [silver].[erp_cust_az12](
-    [cid] [nvarchar](50) NULL,
-    [bdate] [date] NULL,
-    [gen] [nvarchar](50) NULL,
-    [dwh_create_date] [datetime2](7) NULL
-) ON [PRIMARY]
+CREATE TABLE silver.crm_sales_details (
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
 
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
     DROP TABLE silver.erp_loc_a101;
 GO
 
-SET ANSI_NULLS ON
+CREATE TABLE silver.erp_loc_a101 (
+    cid             NVARCHAR(50),
+    cntry           NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
-SET QUOTED_IDENTIFIER ON
+
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
 GO
-CREATE TABLE [silver].[erp_loc_a101](
-    [cid] [nvarchar](50) NULL,
-    [cntry] [nvarchar](50) NULL,
-    [dwh_create_date] [datetime2](7) NULL
-) ON [PRIMARY]
+
+CREATE TABLE silver.erp_cust_az12 (
+    cid             NVARCHAR(50),
+    bdate           DATE,
+    gen             NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
     DROP TABLE silver.erp_px_cat_g1v2;
 GO
 
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [silver].[erp_px_cat_g1v2](
-    [id] [nvarchar](50) NULL,
-    [cat] [nvarchar](50) NULL,
-    [subcat] [nvarchar](50) NULL,
-    [maintenance] [nvarchar](50) NULL,
-    [dwh_create_date] [datetime2](7) NULL
-) ON [PRIMARY]
+CREATE TABLE silver.erp_px_cat_g1v2 (
+    id              NVARCHAR(50),
+    cat             NVARCHAR(50),
+    subcat          NVARCHAR(50),
+    maintenance     NVARCHAR(50),
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
 GO
 
-ALTER TABLE [silver].[crm_prd_info] ADD DEFAULT (getdate()) FOR [dwh_create_date]
-GO
-ALTER TABLE [silver].[crm_sales_details] ADD DEFAULT (getdate()) FOR [dwh_create_date]
-GO
-ALTER TABLE [silver].[erp_cust_az12] ADD DEFAULT (getdate()) FOR [dwh_create_date]
-GO
-ALTER TABLE [silver].[erp_loc_a101] ADD DEFAULT (getdate()) FOR [dwh_create_date]
-GO
-ALTER TABLE [silver].[erp_px_cat_g1v2] ADD DEFAULT (getdate()) FOR [dwh_create_date]
-GO
